7c826dd9f6a60e9c4b157d9880901ccdfd2c1c40
Chris Hahn <chris+digital@chrishahn.me>
2025-07-30 18:48:52 -0400

    docs: add todos

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ import { SocialLinks } from "@/components/social-links"
 import { EmailSignup } from "@/components/email-signup"
 import { Footer } from "@/components/footer"
 
+/**
+ * TODO: Site Texture/Grain
+ * TODO: Cool retro graphic in header or VHS-style effects
+ * TODO: Do something with video section (maybe our logo??? or just a campfire graphic or something...)
+ * TODO: Retro font
+ * TODO: Favicon
+ * TODO: Wire up email signup to some simple kv store
+ */
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white overflow-x-hidden">
